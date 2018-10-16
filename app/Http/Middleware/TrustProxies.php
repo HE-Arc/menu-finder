c5a65760680e6c02e1b318402213ab754e5e1dff
Jordy Crescoli <jordy.crescoli@he-arc.ch>
2018-10-16 21:07:12 +0200

    Fixes mixed contents issue due to the use of a proxy on production
    - Uses TrustProxies middleware to allow the Request class to use the X-FORWARDED-* HTTP headers
    - Creates a new variable X_FORWARDED_TRUSTED_PROXIES in the dot-env file to change the trusted proxies depending on the environment

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use Illuminate\Contracts\Config\Repository;
 use Illuminate\Http\Request;
 use Fideloper\Proxy\TrustProxies as Middleware;
 
@@ -20,4 +21,23 @@ class TrustProxies extends Middleware
      * @var int
      */
     protected $headers = Request::HEADER_X_FORWARDED_ALL;
+
+    /**
+     * Trusted proxies setup to avoid mixed content error on the production server.
+     *
+     * @param Repository $config
+     *
+     * @link https://github.com/HE-Arc/menu-finder/issues/14
+     * @link https://laravel.com/docs/5.7/requests#configuring-trusted-proxies
+     * @link https://github.com/fideloper/TrustedProxy#why-does-this-matter
+     */
+    public function __construct(Repository $config)
+    {
+        parent::__construct($config);
+
+        $proxies = env('X_FORWARDED_TRUSTED_PROXIES');
+        if (!empty($proxies)) {
+            $this->proxies = explode(',', $proxies);
+        }
+    }
 }
