f4165caa9d6915ab89e868daeb478952ed74c789
jules.perrelet1 <jules.perrelet@he-arc.ch>
2018-10-20 15:09:51 +0200

    laravel-postgis avec modèle restaurant

--- app/Restaurant.php
+++ app/Restaurant.php
@@ -3,9 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Phaza\LaravelPostgis\Eloquent\PostgisTrait;
 
 class Restaurant extends Model
 {
+    use PostgisTrait;
+
+
     protected $fillable = [
     'name',
     'active',
@@ -16,6 +20,28 @@ class Restaurant extends Model
     'zip',
     'city',
     'avatar',
-    'description'
+    'description',
     ];
+
+    protected $postgisFields = [
+    'location',
+    ];
+
+  /**
+  * Retrieves all restaurants located within the given radius of a given point.
+  * @param double $lat The latitude of the center point.
+  * @param double $lng The longitude of the center point.
+  * @param double $radius The radius in km.
+  * @return Collection
+  * @link https://postgis.net/docs/ST_DWithin.html PostGIS' ST_DWithin documentation.
+  */
+    public static function allWithinRadius($lat, $lng, $radius)
+    {
+        $query = 'ST_DWithin(position, ST_Point(:lng, :lat)::geography, :radius)';
+        return Restaurant::whereRaw($query, [
+        'lng' => $lng,
+        'lat' => $lat,
+        'radius' => $radius * 1000,
+        ])->get();
+    }
 }
