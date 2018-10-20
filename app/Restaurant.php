157d6c78b0e298d454b729ef56d9cf907131ad7f
jules.perrelet1 <jules.perrelet@he-arc.ch>
2018-10-20 14:54:28 +0200

    preparation des modèles

--- app/Restaurant.php
+++ app/Restaurant.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Restaurant extends Model
 {
-    //
+    protected $fillable = [
+    'name',
+    'active',
+    'rate_sum',
+    'rate_nb',
+    'location',
+    'address',
+    'zip',
+    'city',
+    'avatar',
+    'description'
+    ];
 }
