88b714c3e6d549cf6117fe92f21380219e43c894
Jordy Crescoli <jordy.crescoli@he-arc.ch>
2018-12-18 22:27:12 +0100

    Adds geospatial query to menus index endpoint

--- app/JsonApi/Menus/Adapter.php
+++ app/JsonApi/Menus/Adapter.php
@@ -2,12 +2,14 @@
 
 namespace App\JsonApi\Menus;
 
+use Carbon\Carbon;
 use CloudCreativity\LaravelJsonApi\Eloquent\AbstractAdapter;
 use CloudCreativity\LaravelJsonApi\Eloquent\BelongsTo;
 use CloudCreativity\LaravelJsonApi\Eloquent\HasMany;
 use CloudCreativity\LaravelJsonApi\Pagination\StandardStrategy;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Collection;
+use Neomerx\JsonApi\Contracts\Encoder\Parameters\EncodingParametersInterface;
 
 class Adapter extends AbstractAdapter
 {
@@ -20,13 +22,40 @@ class Adapter extends AbstractAdapter
     protected $attributes = [];
 
     /**
+     * Whitelist of custom filters.
+     * @var array
+     */
+    protected $allowed_custom_filter = [
+        'lat',
+        'lng',
+        'radius',
+    ];
+
+    /**
      * Adapter constructor.
      *
      * @param StandardStrategy $paging
      */
     public function __construct(StandardStrategy $paging)
     {
+        /**
+         * Unfortunately, we did not have time to manage pagination, but it should be easy to implement it
+         * using laraval-json-api package
+         * @link https://laravel-json-api.readthedocs.io/en/latest/fetching/pagination/
+         */
         parent::__construct(new \App\Menu(), $paging);
+    }
+
+    /**
+     * Whitelist the custom attributes used for filtering
+     * @param array $to_whitelist
+     * @return array
+     */
+    protected function whitelistAllowedCustomFilter($to_whitelist)
+    {
+        return array_filter($to_whitelist, function ($key) {
+            return in_array($key, $this->allowed_custom_filter);
+        }, ARRAY_FILTER_USE_KEY);
     }
 
     /**
@@ -36,7 +65,62 @@ class Adapter extends AbstractAdapter
      */
     protected function filter($query, Collection $filters)
     {
-        // TODO
+        /**
+         * By default, we only return menus that are available today.
+         * @todo Add a query parameter to allow the user to query for other day's menus.
+         */
+        $query->where('active', true);
+        $query->where('start', '<=', Carbon::today()->toDateString());
+        $query->where('end', '>=', Carbon::today()->toDateString());
+
+        /**
+         * We can only check for the presence of one geospatial query attribute,
+         * it is up to the validator to make sure that the others are given by the user
+         * @see Validators::queryRules
+         *
+         * @todo Check if it's somehow possible to use the same SQL query everywhere to avoid WET code.
+         * @see \App\Restaurant::allWithinRadius()
+         * @see \App\Menu::allWithinRadius()
+         */
+        if ($filters->has('radius')) {
+            $query->whereHas('restaurant', function ($relation_query) use ($filters) {
+                $sql = 'ST_DWithin(location, ST_Point(?, ?)::geography, ?)';
+                $relation_query->whereRaw($sql, [
+                    $filters->get('lng'),
+                    $filters->get('lat'),
+                    floatval($filters->get('radius')) * 1000,
+                ]);
+            });
+        }
+    }
+
+    /**
+     * Override base class method to add custom query without using json-api "filter." query parameters.
+     * From what we understood, it is better to use custom query parameters when filtering according to an
+     * attribute which doesn't belongs to the model.
+     *
+     * @inheritdoc
+     */
+    protected function queryAll($query, EncodingParametersInterface $parameters)
+    {
+        /** Apply eager loading */
+        $this->with($query, $parameters);
+
+        /** Filter */
+        $filters = collect($parameters->getFilteringParameters());
+        /** Workaround to add our custom query parameters as filters (only whitelîsted custom parameters will pass */
+        if (is_array($parameters->getUnrecognizedParameters())) {
+            $filters = $filters->merge($this->whitelistAllowedCustomFilter($parameters->getUnrecognizedParameters()));
+        }
+        $this->applyFilters($query, $filters);
+
+        /** Sort */
+        $this->sort($query, $parameters->getSortParameters());
+
+        /** Paginate results if needed. */
+        $pagination = collect($parameters->getPaginationParameters());
+
+        return $pagination->isEmpty() ? $this->all($query) : $this->paginate($query, $parameters);
     }
 
     /**
---
+++ app/JsonApi/Menus/Validators.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\JsonApi\Menus;
+
+use CloudCreativity\LaravelJsonApi\Validation\AbstractValidators;
+
+class Validators extends AbstractValidators
+{
+
+    /**
+     * The include paths a client is allowed to request.
+     *
+     * @var string[]
+     */
+    protected $allowedIncludePaths = [
+        'restaurant',
+        'categories',
+        'dishes',
+    ];
+
+    /**
+     * Get resource validation rules.
+     *
+     * @param mixed|null $record
+     *      the record being updated, or null if creating a resource.
+     * @return mixed
+     */
+    protected function rules($record = null): array
+    {
+        return [
+            //
+        ];
+    }
+
+    /**
+     * Get query parameter validation rules.
+     *
+     * @return array
+     */
+    protected function queryRules(): array
+    {
+        return [
+            'lat' => 'numeric|required_with:lng,radius',
+            'lng' => 'numeric|required_with:lat,radius',
+            'radius' => 'numeric|required_with:lat,lng',
+        ];
+    }
+}
